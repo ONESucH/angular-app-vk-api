6259902c174d55d9c22db68044044fffa206a61d
ilya <onesuch07@gmail.com>
2018-02-15 10:30:30 +0300

    Update shablon-user.component.ts

--- src/app/shablon-user/shablon-user.component.ts
+++ src/app/shablon-user/shablon-user.component.ts
@@ -8,7 +8,7 @@ import {Http} from '@angular/http';
 })
 
 export class ShablonUserComponent {
-  public tokken = 'bc33834384bba02bba37ba35d798f4cacd2533350a6e8bd4ac06699b8eac36bf7cc8e628e06218b484fd3';
+  public tokken = 'bc33834384bba02bba398f4cacd2533350a6e8bd4ac06699b8eac36bf7cc8e628e06218b484fd3';
   public url = 'https://api.vk.com/method/users.search?q=&sort=1&offset&count=1000&city1=&country=1&fields=photo_id,verified,sex,bdate,city,country,home_town,photo_100,photo_200,photo_max,online,lists,nickname,occupation,domain,has_mobile,contacts,site,educc&access_token=' + this.tokken + '&v=5.69';
   arr: any[];
 
